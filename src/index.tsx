6b71a6f2d948d73d37b13f59fbd16ada2091c81d
bao_le <le.giabao@ecloudvalley.com>
2024-07-26 16:05:42 +0800

    header style: remove bottom border line

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { HomeScreen } from "./components/HomeScreen/HomeScreen";
 import { ProfileScreen } from "./components/ProfileScreen/ProfileScreen";
 import { NavBackButton } from "./components/common/HeaderNavbarBack/styles";
 import { HeaderNavbarBackButton } from "./components/common/HeaderNavbarBack/HeaderNavBackButton";
-import { Button } from "react-native";
+import { Button } from "react-native-paper";
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -21,11 +21,15 @@ export default function App() {
             name="Profile"
             component={ProfileScreen}
             options={({ navigation }) => ({
+              headerTitle: "",
               headerStyle: {
-                backgroundColor: "#0a0c0e",
+                backgroundColor: "black",
+                // shadowOpacity: 0,
+                // headerShadowVisible: false,
+                borderBottomWidth: 0,
               },
               headerLeft: () => (
-                <NavBackButton onPress={() => navigation.navigate("Home")}/>
+                <Button icon="chevron-left" children={""} onPress={() => navigation.navigate("Home")}/>
               ),
             })}
           />
