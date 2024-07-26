b7a82bab7752c4662313bf33e0762470a01c5df4
bao_le <le.giabao@ecloudvalley.com>
2024-07-26 17:46:26 +0800

    header bar: rounded button

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { ProfileScreen } from "./components/ProfileScreen/ProfileScreen";
 import { NavBackButton } from "./components/common/HeaderNavbarBack/styles";
 import { HeaderNavbarBackButton } from "./components/common/HeaderNavbarBack/HeaderNavBackButton";
 import { Button } from "react-native-paper";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -29,7 +30,24 @@ export default function App() {
                 borderBottomWidth: 0,
               },
               headerLeft: () => (
-                <Button icon="chevron-left" children={""} onPress={() => navigation.navigate("Home")}/>
+                <Button 
+                  // icon="chevron-left" 
+                  icon={() => <Icon name="chevron-left" size={30} color="white"/>}
+                  style={{
+                    width: 64,
+                    height: 64,
+                    // opacity: 0.3,
+                    borderRadius: 32, // Half of width and height to make it round
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginLeft: 16,
+                    marginTop: 48,
+                    backgroundColor: 'gray',
+                  }}
+                  // contentStyle={{ flexDirection: 'row-reverse' }}
+                  children={""} 
+                  onPress={() => navigation.navigate("Home")}
+                />
               ),
             })}
           />
